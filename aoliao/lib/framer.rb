b5123e312d528c088f8146aa48367f04cff4abb8
Xiaofeng Wang <xfwang@thoughtworks.com>
2011-10-07 16:08:40 +0800

    Refactor: remove Framer#text and make Framer#text= a private method

--- aoliao/lib/framer.rb
+++ aoliao/lib/framer.rb
@@ -2,22 +2,18 @@ class Framer
 
   def decorate(*strings)
     self.text = strings
-    self.text.collect do |row|
+    @text.collect do |row|
       "|" + row.ljust(@width) + "|"
     end.unshift(header).push(footer)
   end
 
-  def text
-    @text
-  end
+  private
 
   def text=(strings)
     strings.delete("")
     @text = strings.flatten.compact
     @width = @text.collect { |row| row.size }.max || 0
   end
-
-  private
 
   def header
     footer
