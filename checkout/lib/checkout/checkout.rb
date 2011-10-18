3025716f4cfb8c19eff6ca78aa51a0a492555b4e
Xiaofeng Wang <xfwang@thoughtworks.com>
2011-10-18 22:08:48 +0800

    Moved logic of calculating the total price into new private method and called it in #scan

--- checkout/lib/checkout/checkout.rb
+++ checkout/lib/checkout/checkout.rb
@@ -1,28 +1,32 @@
 class Checkout
+  attr_reader :total_price
 
   def initialize(pricing_rules)
     @pricing_rules = pricing_rules
     @goods = []
-  end
-
-  def total_price
-    goods_list = @goods.join
-    result = 0
-    goods_list.cycle(@goods.count) do |list|
-      @pricing_rules.each do |rule|
-        if list.include?(rule.keys.first.to_s)
-          list.sub!(rule.keys.first.to_s, "")
-          result += rule.values.first
-          break
-        end
-      end
-    end
-    result
+    @total_price = 0
   end
 
   def scan(item)
     @goods << item
     @goods.sort!
+    calculate_price
+  end
+
+  private
+
+  def calculate_price
+    @total_price = 0
+    goods_list = @goods.join
+    goods_list.cycle(@goods.count) do |list|
+      @pricing_rules.each do |rule|
+        if list.include?(rule.keys.first.to_s)
+          list.sub!(rule.keys.first.to_s, "")
+          @total_price += rule.values.first
+          break
+        end
+      end
+    end
   end
 end
 
