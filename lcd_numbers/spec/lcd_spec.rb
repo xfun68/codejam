298bcf71e6c75b032d7533882c0d9ff462a1fdc3
Xiaofeng Wang <xfwang@thoughtworks.com>
2011-10-22 22:02:55 +0800

    LCD: dislay 03

--- lcd_numbers/spec/lcd_spec.rb
+++ lcd_numbers/spec/lcd_spec.rb
@@ -5,9 +5,22 @@ describe LCDNumbers do
   describe "#display" do
     subject { LCDNumbers.new.display(numbers) }
 
-    context "when the numbers is empty" do
-      let(:numbers) { "" }
-      it { should eql("") }
+    context "given only one single number" do
+      let(:numbers) { "0" }
+      it { should eql([ " - ",
+                        "| |",
+                        "   ",
+                        "| |",
+                        " - " ]) }
+    end
+
+    context "given multiple numbers" do
+      let(:numbers) { "03" }
+      it { should eql([ " -  - ",
+                        "| |  |",
+                        "    - ",
+                        "| |  |",
+                        " -  - " ]) }
     end
   end
 end
