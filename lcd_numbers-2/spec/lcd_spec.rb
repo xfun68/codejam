a8d68b752fdd236c0a8bd04eaa7261d755ab9d14
Xiaofeng Wang <xfwang@thoughtworks.com>
2011-10-23 20:59:54 +0800

    LCD-2: display single number

---
+++ lcd_numbers-2/spec/lcd_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/lcd'
+
+describe LCD do
+
+  describe "#display" do
+    subject { LCD.new.display(numbers) }
+
+    context "given a single number" do
+      let(:numbers) { "0" }
+      it { should eql([ " - ",
+                        "| |",
+                        "   ",
+                        "| |",
+                        " - "]) }
+    end
+  end
+end
+
