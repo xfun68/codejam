dc4b9aa8655d3ecd3c40279dfe7f7cda307f0da7
Xiaofeng Wang <xfwang@thoughtworks.com>
2011-11-16 18:53:04 +0800

    Fizz: 3rd step

--- fizz_buzz/lib/fizz_buzz.rb
+++ fizz_buzz/lib/fizz_buzz.rb
@@ -3,10 +3,10 @@ class FizzBuzz
   def count(numbers)
     numbers.map do |number|
       fizz_buzz = ""
-      if fizz_number?(number)
+      if fizz_buzz?(number, 3)
         fizz_buzz += "fizz"
       end
-      if buzz_number?(number)
+      if fizz_buzz?(number, 5)
         fizz_buzz += "buzz"
       end
       fizz_buzz.empty? ? number : fizz_buzz
@@ -15,12 +15,10 @@ class FizzBuzz
 
   private
 
-  def fizz_number?(number)
-    (number % 3) == 0
-  end
-
-  def buzz_number?(number)
-    (number % 5) == 0
+  def fizz_buzz?(number, key_number)
+    (number % key_number) == 0 or
+    number.to_s.include?(key_number.to_s) or
+    (number.to_s.chars.inject(0) { |total, char| total + char.to_i } % key_number == 0)
   end
 end
 
--- fizz_buzz/spec/fizz_rspec.rb
+++ fizz_buzz/spec/fizz_rspec.rb
@@ -5,19 +5,39 @@ describe FizzBuzz do
   describe "#count" do
     subject { FizzBuzz.new.count(numbers) }
 
-    context "with fizz only" do
+    context "with numbers divided by fizz" do
       let(:numbers) { [1, 2, 3] }
       it { should eql([1, 2, "fizz"]) }
     end
 
-    context "with buzz only" do
-      let(:numbers) { [4, 5] }
-      it { should eql([4, "buzz"]) }
+    context "with numbers divided by buzz" do
+      let(:numbers) { [10, 11] }
+      it { should eql(["buzz", 11]) }
     end
 
-    context "with fizz-buzz" do
-      let(:numbers) { [12, 13, 14, 15, 16] }
-      it { should eql(["fizz", 13, 14, "fizzbuzz", 16]) }
+    context "with numbers divided by fizz and buzz" do
+      let(:numbers) { [15, 16] }
+      it { should eql(["fizzbuzz", 16]) }
+    end
+
+    context "with numbers including fizz" do
+      let(:numbers) { [11, 12, 13] }
+      it { should eql([11, "fizz", "fizz"]) }
+    end
+
+    context "with numbers including buzz" do
+      let(:numbers) { [58, 59] }
+      it { should eql(["buzz", "buzz"]) }
+    end
+
+    context "with numbers sum divided by fizz" do
+      let(:numbers) { [21, 22] }
+      it { should eql(["fizz", 22]) }
+    end
+
+    context "with all conditions" do
+      let(:numbers) { [555] }
+      it { should eql(["fizzbuzz"]) }
     end
   end
 end
