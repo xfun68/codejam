fbceffb67b647a40b4d78bae53f70920db32ca39
Xiaofeng Wang <xfwang@thoughtworks.com>
2011-11-16 20:36:16 +0800

    Fizz: 3rd step REFACTOR

--- fizz_buzz/lib/fizz_buzz.rb
+++ fizz_buzz/lib/fizz_buzz.rb
@@ -1,24 +1,31 @@
+class Cipher
+
+  def initialize(target_number, password)
+    @target_number = target_number
+    @password = password
+  end
+
+  def encrypt(number)
+    ((number % @target_number) == 0 or
+     number.to_s.include?(@target_number.to_s) or
+     (number.to_s.chars.inject(0) { |total, char| total + char.to_i } % @target_number == 0)) ? @password : ""
+  end
+end
+
 class FizzBuzz
+
+  def initialize
+    @ciphers = [Cipher.new(3, "fizz"), Cipher.new(5, "buzz")]
+  end
 
   def count(numbers)
     numbers.map do |number|
       fizz_buzz = ""
-      if fizz_buzz?(number, 3)
-        fizz_buzz += "fizz"
-      end
-      if fizz_buzz?(number, 5)
-        fizz_buzz += "buzz"
+      @ciphers.each do |cipher|
+        fizz_buzz += cipher.encrypt(number)
       end
       fizz_buzz.empty? ? number : fizz_buzz
     end
-  end
-
-  private
-
-  def fizz_buzz?(number, key_number)
-    (number % key_number) == 0 or
-    number.to_s.include?(key_number.to_s) or
-    (number.to_s.chars.inject(0) { |total, char| total + char.to_i } % key_number == 0)
   end
 end
 
